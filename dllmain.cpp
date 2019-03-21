372c5d12e333f925c4d92c18edd56e3c8f1882c4
nomnomab <nomnomab1@gmail.com>
2019-03-21 19:45:14 -0400

    Local mod folder support

--- dllmain.cpp
+++ dllmain.cpp
@@ -9,16 +9,26 @@
 BOOL APIENTRY DllMain( HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved) {
 	if (ul_reason_for_call == DLL_PROCESS_ATTACH) {
 		MessageBoxA(NULL, "Attempting to load mod DLLs!", "Satisfactory Mod Loader", NULL);
-		std::string path = "C:\\dev\\asm\\dll";
-		std::string pathExact = "C:\\dev\\asm\\dll\\";
+
+		// get application path
+		char p[MAX_PATH];
+		GetModuleFileNameA(NULL, p, MAX_PATH);
+
+		// split the path
+		std::string appPath(p);
+		size_t pos = appPath.find_last_of('\\');
+		std::string path = appPath.substr(0, pos) + "\\mods";
+		std::string pathExact = path + "\\";
+
 		for (const auto & entry : std::experimental::filesystem::directory_iterator(path)) {
-			if (entry.path().extension() == std::experimental::filesystem::path("test.dll").extension()) {
+			if (entry.path().extension().string() == ".dll") {
 				std::string file = pathExact.append(entry.path().filename().string());
 				std::wstring stemp = std::wstring(file.begin(), file.end());
 				LPCWSTR sw = stemp.c_str();
-				LoadLibrary(sw);
+				HMODULE mod = LoadLibrary(sw);
+				
 				//std::cout << GetLastError();
-				pathExact = "C:\\dev\\asm\\dll\\";
+				//pathExact = "C:\\dev\\asm\\dll\\";
 			}
 		}
 	}
