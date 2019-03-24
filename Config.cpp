8ff5230a747163183e080e1c5130a385fe8910fa
nomnomab <nomnomab1@gmail.com>
2019-03-24 16:54:44 -0400

    Fixed name

--- Config.cpp
+++ Config.cpp
@@ -1,5 +1,6 @@
-#include "Config.h"
 #include "stdafx.h"
+#include "Config.h"
+#include "DllMain.h"
 #include <string>
 #include <iostream>
 #include <fstream>
@@ -7,20 +8,20 @@
 bool LOADCONSOLE = true;
 
 void readConfig() {
-	std::cout << "[SML] Finding config file..." << std::endl;
+	log("Finding config file...");
 	std::ifstream config("config.cfg");
 	std::string line;
 	if (config.good()) { // see if a config file exists
 		if (config.is_open()) {
-			std::cout << "[SML] Reading found config file!" << std::endl;
+			log("Reading the config file!");
 			while (getline(config, line)) { // read the config file line by linee
 				size_t seperator = line.find(":"); // separate string into a id/value pair
-				std::string id = line.substr(0, seperator); 
-				std::string value = line.substr(seperator+1, std::string::npos);
+				std::string id = line.substr(0, seperator);
+				std::string value = line.substr(seperator + 1, std::string::npos);
 				if (id == "Console") {
 					if (value == "false") { //see if console is false
 						LOADCONSOLE = false;
-						std::cout << "[SML] You are free to close this window now." << std::endl;
+						log("You are free to close this window now.");
 					}
 					else {
 						LOADCONSOLE = true;
@@ -28,8 +29,10 @@ void readConfig() {
 				}
 			}
 		}
-	} else { // create a config file if does not exist
-		std::cout << "[SML] No config file found!\nCreating new config file..." << std::endl;
+	}
+	else { // create a config file if does not exist
+		log("No config file found!");
+		log("Creating new config file...");
 		std::ofstream configOut("config.cfg");
 		if (configOut.is_open()) {
 			configOut << "Console:true";
