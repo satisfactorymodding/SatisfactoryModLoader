310645b86d33942f946c5a75a9e0f760317cfde4
Antti Tuppurainen <antttirt@anttirt.net>
2019-04-21 17:08:27 +0300

    Remove separate xinput1_3 project, add detours submodule, build SatisfactoryModLoader -> xinput1_3.dll

---
+++ SatisfactoryModLoader/util/DetoursFwd.h
@@ -0,0 +1,19 @@
+#pragma once
+
+namespace Detours {
+	extern "C" {
+		// LONG WINAPI DetourTransactionBegin(VOID);
+		void __stdcall DetourTransactionBegin();
+
+		// PVOID WINAPI DetourFindFunction(_In_ LPCSTR pszModule,
+		//                                 _In_ LPCSTR pszFunction);
+		void* __stdcall DetourFindFunction(const char* pszModule, const char* pszFunction);
+
+		// LONG WINAPI DetourAttach(_Inout_ PVOID *ppPointer,
+		//                          _In_ PVOID pDetour);
+		long __stdcall DetourAttach(void** ppPointer, void* pDetour);
+
+		// LONG WINAPI DetourTransactionCommit(VOID);
+		void __stdcall DetourTransactionCommit();
+	}
+}
