01d738fcb8da35fd2e1aed297199faf579348c9c
SuperCoder79 <k.pranav@gmail.com>
2019-06-22 03:49:45 -0700

    Updated version

--- SatisfactoryModLoader/SatisfactoryModLoader.h
+++ SatisfactoryModLoader/SatisfactoryModLoader.h
@@ -8,7 +8,7 @@
 
 namespace SML {
 	static const std::string modLoaderVersion = "1.0.0-pr4"; // SML's version
-	static const std::string targetVersion[] = { "99427", "100692" }; //CLs of Satisfactory, 1st is normal CL and 2nd is experimental CL
+	static const std::string targetVersion[] = { "99427", "100979" }; //CLs of Satisfactory, 1st is normal CL and 2nd is experimental CL
 	static bool loadConsole = true;
 	static bool debugOutput = false;
 	static bool supressErrors = false;
