63410773f7d0da3816d19fae6a49aa74e3e9e250
SuperCoder7979 <k.pranav@gmail.com>
2019-10-25 11:17:45 -0400

    bugfix

--- SatisfactoryModLoader/mod/ModHandler.cpp
+++ SatisfactoryModLoader/mod/ModHandler.cpp
@@ -149,16 +149,11 @@ namespace SML {
 					//check if modloader's version is the same as the mod's target version
 					size_t modTVOffset = mod->info.loaderVersion.find_last_of(".");
 					size_t SMLOffset = modLoaderVersion.find_last_of(".");
-					if (supressErrors) {
-						if (!(mod->info.loaderVersion.substr(0, modTVOffset) == modLoaderVersion.substr(0, SMLOffset))) {
+					if (!(mod->info.loaderVersion.substr(0, modTVOffset) == modLoaderVersion.substr(0, SMLOffset))) {
+						if (!supressErrors) {
 							std::string msg = mod->info.name + " does not match SML's version! Please ask the mod developer (" + mod->info.authors + ") to update their mod. Press OK to continue mod loading.";
 							MessageBoxA(NULL, msg.c_str(), "SatisfactoryModLoader Warning", MB_ICONWARNING);
-							if (!unsafeMode) {
-								FreeLibrary(dll);
-							}
-							continue;
 						}
-					} else {
 						if (!unsafeMode) {
 							FreeLibrary(dll);
 						}
