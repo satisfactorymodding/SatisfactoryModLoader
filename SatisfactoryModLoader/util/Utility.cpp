0830170939eb2fcbc094d92b9678d9e8a1fe5e15
SuperCoder7979 <k.pranav@gmail.com>
2019-10-26 15:59:03 -0400

    seperate logging from utility

--- SatisfactoryModLoader/util/Utility.cpp
+++ SatisfactoryModLoader/util/Utility.cpp
@@ -10,48 +10,6 @@
 
 namespace SML {
 	namespace Utility {
-		std::ofstream logFile;
-
-		void log() {}
-
-		void log(LogType type) {}
-
-		void writeHeader(std::string header, LogType type) {
-			std::string logType;
-
-			switch (type) {
-			case Debug:
-			case ModDebug:
-				logType = "Debug]   ";
-				break;
-			case Info:
-			case ModInfo:
-				logType = "Info]    ";
-				break;
-			case Warning:
-			case ModWarning:
-				logType = "Warning] ";
-				break;
-			case Error:
-			case ModError:
-				logType = "Error]   ";
-				break;
-			}
-
-			// log line
-			logFile << "[" + header + "::" + logType;
-			logFile.flush();
-
-			// cout line
-			setConsoleColor(type > 2 && type <= 5 ? ConsoleColor::Cyan : ConsoleColor::White);
-			std::cout << "[" << header << "] ";
-		}
-
-		void setConsoleColor(ConsoleColor color) {
-			HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-			// sets the console color
-			SetConsoleTextAttribute(hConsole, color + 1);
-		}
 
 		//target[0] = normal CL, target [1] = experimental CL
 		void checkVersion(const int target) {
