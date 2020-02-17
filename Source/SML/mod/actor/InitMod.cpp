3b19d75e52586af2da41f10a8ae92ac9796ba06f
mircearoata <mircearoatapalade@yahoo.ro>
2020-02-17 19:57:43 +0200

    Fixed ExampleMod schematic

--- Source/SML/mod/actor/InitMod.cpp
+++ Source/SML/mod/actor/InitMod.cpp
@@ -1,12 +1,15 @@
 #include "InitMod.h"
 #include "FGSchematicManager.h"
+#include "SML/util/Logging.h"
 
 void AInitMod::LoadSchematics() {
 	AFGSchematicManager* schematicManager = AFGSchematicManager::Get(this);
 	for (TSubclassOf<UFGSchematic> schematic : mSchematics) {
 		TArray<TSubclassOf<UFGSchematic>> availableSchematics;
 		schematicManager->GetAvailableSchematics(availableSchematics);
+		SML::Logging::debug("Loading schematic ", *UFGSchematic::GetSchematicDisplayName(schematic).ToString(), " of mod ", *this->GetClass()->GetPathName());
 		if (!schematicManager->IsSchematicPurchased(schematic) && !availableSchematics.Contains(schematic)) {
+			SML::Logging::debug("Adding schematic ", *UFGSchematic::GetSchematicDisplayName(schematic).ToString(), " of mod ", *this->GetClass()->GetPathName());
 			schematicManager->AddAvailableSchematic(schematic);
 		}
 	}
