953a9aea667d6e6b1d360d17f19cab442b63c158
mircearoata <mircearoatapalade@yahoo.ro>
2020-02-26 23:07:00 +0200

    Added modding edits back

--- Source/FactoryGame/Public/FGChatManager.h
+++ Source/FactoryGame/Public/FGChatManager.h
@@ -32,6 +32,8 @@ public:
 
 	FChatMessageStruct();
 	FChatMessageStruct( FString messageString, class AFGPlayerState* sender, float serverTimeStamp );
+	//MODDING EDIT: default destructor is empty and doesn't exist in game EXE, so we should force inline it.
+	__forceinline ~FChatMessageStruct() = default;
 
 	/** The message that was sent */
 	UPROPERTY( BlueprintReadWrite )
@@ -71,7 +73,8 @@ public:
 
 public:
 	/** Get the chat manager, this should always return something unless you call it really early. */
-	static AFGChatManager* Get( UWorld* world );
+	//MODDING EDIT: doesn't exist in executable, so forceinline to use version below
+	__forceinline static AFGChatManager* Get(UWorld* world) { return Get(static_cast<UObject*>(world)); }
 
 	/** Get the chat manager from a world context, this should always return something unless you call it really early. */
 	UFUNCTION( BlueprintPure, Category = "Schematic", DisplayName = "GetChatManager", Meta = ( DefaultToSelf = "worldContext" ) )
