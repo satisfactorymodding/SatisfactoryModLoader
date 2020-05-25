c69aa734a1bf1869a1d78a2e6ceb8fbf90fda875
Dragon2488 <Archengius@gmail.com>
2020-05-26 03:31:14 +0700

    ExampleMod: ExampleBuilding with ReplicationDetailActor usage example

--- Source/ExampleMod/ExampleModModule.cpp
+++ Source/ExampleMod/ExampleModModule.cpp
@@ -1,14 +1,8 @@
 #include "ExampleModModule.h"
-#include "ExampleActor.h"
-#include "../SML/mod/hooking.h"
-#include "FGGameMode.h"
-#include <fstream>
+#include "mod/blueprint_hooking.h"
+#include "util/Logging.h"
 
 void FExampleModModule::StartupModule() {
-	SUBSCRIBE_METHOD("?InitGameState@AFGGameMode@@UEAAXXZ", AFGGameMode::InitGameState, [](auto& scope, AFGGameMode* gameMode) {
-		AExampleActor* actor = gameMode->GetWorld()->SpawnActor<AExampleActor>(FVector::ZeroVector, FRotator::ZeroRotator);
-		actor->DoStuff();
-	});
 }
 
 IMPLEMENT_GAME_MODULE(FExampleModModule, ExampleMod);
