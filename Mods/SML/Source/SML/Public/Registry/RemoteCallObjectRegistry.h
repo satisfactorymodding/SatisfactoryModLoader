921366ff45270d8cc2e315d4041d25f89f5ccc87
Robb <computerguy440+gh@gmail.com>
2023-06-24 13:28:39 -0500

    Add comment to RegisterRCO function suggesting usage of the GameInstanceModule array instead

--- Mods/SML/Source/SML/Public/Registry/RemoteCallObjectRegistry.h
+++ Mods/SML/Source/SML/Public/Registry/RemoteCallObjectRegistry.h
@@ -8,6 +8,10 @@ UCLASS()
 class SML_API URemoteCallObjectRegistry : public UGameInstanceSubsystem {
     GENERATED_BODY()
 public:
+    /**
+     * Manually register a remote call object (RCO).
+     * In most cases you should add the RCO to the RemoteCallObjects array of a Game Instance Subsystem instead
+     */
     UFUNCTION(BlueprintCallable)
     void RegisterRemoteCallObject(TSubclassOf<UFGRemoteCallObject> RemoteCallObject);
 
