6eeb88c220df71ee108eb4950f5f7d0d5c6978e5
mircearoata <mircearoatapalade@gmail.com>
2023-07-04 14:34:19 +0200

    Add AccessTransformers plugin

---
+++ Mods/AccessTransformers/Source/AccessTransformers/Types/AccessTransformers.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using EpicGames.Core;
+using EpicGames.UHT.Types;
+using UnrealBuildBase;
+using UnrealBuildTool;
+
+namespace AccessTransformers.Types;
+
+public static class AccessTransformers
+{
+    public abstract class BaseAccessTransformer
+    {
+        public readonly string File;
+        public readonly int Line;
+
+        protected BaseAccessTransformer(string file, int line)
+        {
+            File = file;
+            Line = line;
+        }
+        
+        public abstract string Class { get; }
+        public abstract List<string>? GetImplementation(UhtType type);
+    }
+    public class FriendAccessTransformer : BaseAccessTransformer
+    {
+        private readonly string _className;
+        private readonly string _friendClassName;
+
+        public FriendAccessTransformer(string className, string friendClassName, string file, int line) : base(file ,line)
+        {
+            _className = className;
+            _friendClassName = friendClassName;
+        }
+        
+        public override string ToString()
+        {
+            return $"{_className} -> {_friendClassName}";
+        }
+
+        public override string Class => _className;
+        public override List<string>? GetImplementation(UhtType type)
+        {
+            return new List<string> { "friend class " + _friendClassName + ";" };
+        }
+    }
+
+    public class AccessorAccessTransformer : BaseAccessTransformer
+    {
+        private readonly string _className;
+        private readonly string _fieldName;
+        
+        public AccessorAccessTransformer(string className, string fieldName, string file, int line) : base(file ,line)
+        {
+            _className = className;
+            _fieldName = fieldName;
+        }
+        
+        public override string ToString()
+        {
+            return $"{_className}:{_fieldName}";
+        }
+
+        public override string Class => _className;
+        public override List<string>? GetImplementation(UhtType type)
+        {
+            var property = (UhtProperty?)type.Children.Find((child) => child.EngineName == _fieldName && child.EngineType == UhtEngineType.Property);
+            if (property == null)
+                return null;
+
+
+            StringBuilder getterBuilder = new();
+            property.AppendText(getterBuilder, UhtPropertyTextType.GenericFunctionArgOrRetVal);
+            getterBuilder.Append(' ').Append("Get").Append(property.SourceName).Append("() const { return ").Append(property.SourceName).Append("; }");
+            
+            StringBuilder setterBuilder = new();
+            setterBuilder.Append("void Set").Append(property.SourceName).Append($"(");
+            property.AppendText(setterBuilder, UhtPropertyTextType.GenericFunctionArgOrRetVal);
+            setterBuilder.Append(" value) { ").Append(property.SourceName).Append(" = value; }");
+            
+            return new List<string> { getterBuilder.ToString(), setterBuilder.ToString() };
+        }
+    }
+
+    public static readonly AccessTransformerTable<FriendAccessTransformer> FriendAccessTransformers = new();
+    public static readonly AccessTransformerTable<AccessorAccessTransformer> AccessorAccessTransformers = new();
+
+    public static void Load(string projectFile)
+    {
+        var availablePlugins = PluginsBase.EnumeratePlugins(new FileReference(projectFile));
+        foreach (var plugin in availablePlugins)
+        {
+            FileReference accessTransformersFile = new(Path.Combine(plugin.Directory.FullName, "Config", "AccessTransformers.ini"));
+            if (accessTransformersFile.ToFileInfo().Exists)
+            {
+                var accessTransformers = new ConfigFile(accessTransformersFile, ConfigLineAction.Add);
+                accessTransformers.TryGetSection("AccessTransformers", out var section);
+                section!.Lines.ForEach((line) =>
+                {
+                    var data = StructParser.Parse(line.Value);
+                    switch (line.Key)
+                    {
+                        case "Friend":
+                            FriendAccessTransformers.Add(plugin.GetFileNameWithoutAnyExtensions(),
+                                new FriendAccessTransformer(data["Class"], data["FriendClass"], accessTransformersFile.FullName, section.Lines.IndexOf(line) + 1));
+                            break;
+                        case "Accessor":
+                            AccessorAccessTransformers.Add(plugin.GetFileNameWithoutAnyExtensions(),
+                                new AccessorAccessTransformer(data["Class"], data["Property"], accessTransformersFile.FullName, section.Lines.IndexOf(line) + 1));
+                            break;
+                    }
+                });
+            }
+        }
+    }
+}
---
+++ Mods/AccessTransformers/Source/AccessTransformers/Types/IniParsing.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AccessTransformers.Types;
+
+public class StructParser
+{
+    private int _index;
+    private readonly string _text;
+
+    private StructParser(string text)
+    {
+        _text = text;
+        _index = 0;
+    }
+
+    public static Dictionary<string, string> Parse(string text)
+    {
+        return new StructParser(text).Parse();
+    }
+
+    private Dictionary<string, string> Parse()
+    {
+        Dictionary<string, string> result = new();
+        Required("(");
+        while(_text[_index] != ')')
+        {
+            var key = ReadSymbol();
+            Required("=");
+            var quoted = Optional("\"");
+            var value = ReadSymbol();
+            if (quoted)
+                Required("\"");
+            SkipSpace();
+            result.Add(key, value);
+            if(!Optional(","))
+                break;
+        }
+        Required(")");
+        return result;
+    }
+
+    private string ReadSymbol()
+    {
+        SkipSpace();
+        var result = "";
+        while(_index < _text.Length && char.IsLetterOrDigit(_text[_index]))
+        {
+            result += _text[_index];
+            _index++;
+        }
+        if (result == "")
+        {
+            throw new Exception("Expected symbol at " + _index);
+        }
+        SkipSpace();
+        return result;
+    }
+
+    private void SkipSpace()
+    {
+        while (_index < _text.Length && char.IsWhiteSpace(_text[_index]))
+        {
+            _index++;
+        }
+    }
+
+    private bool Optional(string s)
+    {
+        if (!_text[_index..].StartsWith(s))
+            return false;
+        _index += s.Length;
+        SkipSpace();
+        return true;
+    }
+    
+    private void Required(string s)
+    {
+        if (!_text[_index..].StartsWith(s))
+        {
+            throw new Exception("Expected " + s + " at " + _index);
+        }
+        _index += s.Length;
+        SkipSpace();
+    }
+}
