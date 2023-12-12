b769b379065141b04c8e6665f9015ba14a864938
Rob B <computerguy440+gh@gmail.com>
2023-12-12 00:57:49 -0600

    Update alpakit CPPAndBlueprintBlank template

--- Mods/Alpakit/Templates/CPPAndBlueprintBlank/Source/PLUGIN_NAME/PLUGIN_NAME.Build.cs
+++ Mods/Alpakit/Templates/CPPAndBlueprintBlank/Source/PLUGIN_NAME/PLUGIN_NAME.Build.cs
@@ -1,53 +1,84 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
 using UnrealBuildTool;
+using System.IO;
+using System;
 
 public class PLUGIN_NAME : ModuleRules
 {
 	public PLUGIN_NAME(ReadOnlyTargetRules Target) : base(Target)
 	{
-		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		// FactoryGame transitive dependencies
+		// Not all of these are required, but including the extra ones saves you from having to add them later.
+		// Some entries are commented out to avoid compile-time warnings about depending on a module that you don't explicitly depend on.
+		// You can uncomment these as necessary when your code actually needs to use them.
+		PublicDependencyModuleNames.AddRange(new string[] {
+			"Core", "CoreUObject",
+			"Engine",
+			"DeveloperSettings",
+			"PhysicsCore",
+			"InputCore",
+			//"OnlineSubsystem", "OnlineSubsystemUtils", "OnlineSubsystemNull",
+			//"SignificanceManager",
+			"GeometryCollectionEngine",
+			//"ChaosVehiclesCore", "ChaosVehicles", "ChaosSolverEngine",
+			"AnimGraphRuntime",
+			//"AkAudio",
+			"AssetRegistry",
+			"NavigationSystem",
+			//"ReplicationGraph",
+			"AIModule",
+			"GameplayTasks",
+			"SlateCore", "Slate", "UMG",
+			//"InstancedSplines",
+			"RenderCore",
+			"CinematicCamera",
+			"Foliage",
+			//"Niagara",
+			//"EnhancedInput",
+			//"GameplayCameras",
+			//"TemplateSequence",
+			"NetCore",
+			"GameplayTags",
+			"Json", "JsonUtilities",
+			"AssetRegistry"
+		});
+
+		// FactoryGame plugins
+		PublicDependencyModuleNames.AddRange(new string[] {
+			//"AbstractInstance",
+			//"InstancedSplinesComponent",
+			//"SignificanceISPC"
+		});
+
+		// Header stubs
+		PublicDependencyModuleNames.AddRange(new string[] {
+			"DummyHeaders",
+		});
+
+		if (Target.Type == TargetRules.TargetType.Editor) {
+			PublicDependencyModuleNames.AddRange(new string[] {/*"OnlineBlueprintSupport",*/ "AnimGraph"});
+		}
+		PublicDependencyModuleNames.AddRange(new string[] {"FactoryGame", "SML"});
 		
-		PublicIncludePaths.AddRange(
-			new string[] {
-				// ... add public include paths required here ...
-			}
-			);
-				
+		PublicIncludePaths.AddRange(new string[] {
+			// ... add public include paths required here ...
+		});
 		
-		PrivateIncludePaths.AddRange(
-			new string[] {
-				// ... add other private include paths required here ...
-			}
-			);
-			
+		PrivateIncludePaths.AddRange(new string[] {
+			// ... add private include paths required here ...
+		});
 		
-		PublicDependencyModuleNames.AddRange(
-			new string[]
-			{
-				"Core",
-				// ... add other public dependencies that you statically link with here ...
-			}
-			);
-			
+		PublicDependencyModuleNames.AddRange(new string[] {
+			// ... add public dependencies that you statically link with here ...
+		});
 		
-		PrivateDependencyModuleNames.AddRange(
-			new string[]
-			{
-				"CoreUObject",
-				"Engine",
-				"Slate",
-				"SlateCore",
-				// ... add private dependencies that you statically link with here ...	
-			}
-			);
+		PrivateDependencyModuleNames.AddRange(new string[] {
+			// ... add private dependencies that you statically link with here ...	
+		});
 		
-		
-		DynamicallyLoadedModuleNames.AddRange(
-			new string[]
-			{
-				// ... add any modules that your module loads dynamically here ...
-			}
-			);
+		DynamicallyLoadedModuleNames.AddRange(new string[] {
+			// ... add any modules that your module loads dynamically here ...
+		});
 	}
 }
