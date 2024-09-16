d9a389c9cdfc1e593db4b51e13a8c43f990feb05
Rob B <computerguy440+gh@gmail.com>
2024-09-16 19:50:27 -0400

    Add developer comment data field to Widget Blueprint Hooks

--- Mods/SML/Source/SML/Public/Patching/WidgetBlueprintHookManager.h
+++ Mods/SML/Source/SML/Public/Patching/WidgetBlueprintHookManager.h
@@ -42,6 +42,10 @@ class SML_API UWidgetBlueprintHookData : public UDataAsset {
 public:
 	UWidgetBlueprintHookData();
 	
+	/** Use this field to leave future you notes about what this hook is supposed to accomplish */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Default", meta = (MultiLine = "true"))
+	FString DeveloperComment;
+
 	/** Widget Blueprint to hook */
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Default", BlueprintSetter = SetWidgetClass)
 	TSoftClassPtr<UUserWidget> WidgetClass;
