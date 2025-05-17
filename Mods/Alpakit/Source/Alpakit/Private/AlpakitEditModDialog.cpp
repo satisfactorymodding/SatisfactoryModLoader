04726817ff5512d9a2a354a2b3b59c63cdde8ba6
Mark Lierman <marklierman@hotmail.com>
2025-05-17 14:04:33 -0400

    Add button to convert mod to BP & CPP (#362)
    * Add button to convert mod to BP & CPP
    
    Added button to the Edit Mod dialog to convert the mod to a combined Blueprint & CPP mod
    
    * Update loctext keys. Mention needing to regen project files
    
    ---------
    
    Co-authored-by: Rob B <computerguy440+gh@gmail.com>

--- Mods/Alpakit/Source/Alpakit/Private/AlpakitEditModDialog.cpp
+++ Mods/Alpakit/Source/Alpakit/Private/AlpakitEditModDialog.cpp
@@ -100,12 +100,37 @@ void SAlpakitEditModDialog::Construct(const FArguments& InArgs, TSharedRef<IPlug
 				+ SVerticalBox::Slot()
 				.AutoHeight()
 				.Padding(5)
-				.HAlign(HAlign_Right)
+				.HAlign(HAlign_Fill)
 				[
-					SNew(SButton)
-					.ContentPadding(FMargin(20.0f, 2.0f))
-					.Text(LOCTEXT("OkButtonLabel", "Ok"))
-					.OnClicked(this, &SAlpakitEditModDialog::OnOkClicked)
+					SNew(SHorizontalBox)
+					+ SHorizontalBox::Slot()
+					.AutoWidth()
+					.HAlign(HAlign_Left)
+					[
+						SNew(SButton)
+						.IsEnabled_Lambda([this]() {
+							return !HasCPP();
+						})
+						.ToolTipText_Lambda([this]() {
+							return HasCPP() 
+								? LOCTEXT("AddCppButtonDisabledTooltip", "This mod already has C++ module files.") 
+								: LOCTEXT("AddCppButtonEnabledTooltip", "Create C++ module files to convert this mod into a hybrid Blueprint and C++ mod. This can't be automatically reversed.");
+						})
+						.ContentPadding(FMargin(20.0f, 2.0f))
+						.Text(LOCTEXT("AddCppButtonLabel", "Convert to C++ & Blueprint Mod"))
+						.OnClicked(this, &SAlpakitEditModDialog::OnCPPClicked)
+					]
+					+ SHorizontalBox::Slot()
+					.FillWidth(1.0f)
+					+ SHorizontalBox::Slot()
+					.AutoWidth()
+					.HAlign(HAlign_Right)
+					[
+						SNew(SButton)
+						.ContentPadding(FMargin(20.0f, 2.0f))
+						.Text(LOCTEXT("OkButtonLabel", "Ok"))
+						.OnClicked(this, &SAlpakitEditModDialog::OnOkClicked)
+					]
 				]
 			]
 		]
@@ -159,6 +184,12 @@ FString SAlpakitEditModDialog::FormatGameVersionRange(const FVersionRange& Range
 	return Range.ToString().Replace(TEXT(".0.0"), TEXT(""));
 }
 
+bool SAlpakitEditModDialog::HasCPP()
+{
+	FString SourceFolderPath = Mod->GetBaseDir() / TEXT("Source");
+	return FPaths::DirectoryExists(SourceFolderPath);
+}
+
 FReply SAlpakitEditModDialog::OnOkClicked() {
 	FPluginDescriptor OldDescriptor = Mod->GetDescriptor();
 
@@ -208,6 +239,86 @@ FReply SAlpakitEditModDialog::OnOkClicked() {
 			FMessageDialog::Open(EAppMsgType::Ok, FailReason);
 		}
 	}
+	return FReply::Handled();
+}
+
+FReply SAlpakitEditModDialog::OnCPPClicked()
+{
+	FString SourceDir = Mod->GetBaseDir() / TEXT("Source");
+	FString AlpakitDir = IPluginManager::Get().FindPlugin(TEXT("Alpakit"))->GetBaseDir();
+	FString TemplatesDir = AlpakitDir / TEXT("Templates");
+	FString TemplateSourceDir = TemplatesDir / TEXT("CPPAndBlueprintBlank/Source");
+	FString ModName = Mod->GetName();
+
+	// Create Source directory if it doesn't exist
+	IFileManager& FileManager = IFileManager::Get();
+	if (!FileManager.DirectoryExists(*SourceDir))
+	{
+		FileManager.MakeDirectory(*SourceDir, true);
+	}
+
+	// Copy all files from template Source directory to mod's Source directory
+	TArray<FString> Files;
+	FileManager.FindFilesRecursive(Files, *TemplateSourceDir, TEXT("*.*"), true, false);
+	for (const FString& File : Files)
+	{
+		FString RelativePath = File.RightChop(TemplateSourceDir.Len() + 1);
+		// Replace PLUGIN_NAME in directory and file names
+		RelativePath = RelativePath.Replace(TEXT("PLUGIN_NAME"), *ModName);
+		FString TargetPath = SourceDir / RelativePath;
+		
+		// Create target directory if it doesn't exist
+		FString TargetDir = FPaths::GetPath(TargetPath);
+		if (!FileManager.DirectoryExists(*TargetDir))
+		{
+			FileManager.MakeDirectory(*TargetDir, true);
+		}
+
+		// Read the file content
+		FString FileContent;
+		if (FFileHelper::LoadFileToString(FileContent, *File))
+		{
+			// Replace PLUGIN_NAME in file content
+			FileContent = FileContent.Replace(TEXT("PLUGIN_NAME"), *ModName);
+			
+			// Write the modified content to the target file
+			FFileHelper::SaveStringToFile(FileContent, *TargetPath);
+		}
+	}
+
+	// Modify the .uplugin file to add the module configuration
+	FString PluginDescriptorPath = Mod->GetDescriptorFileName();
+	FString PluginContent;
+	if (FFileHelper::LoadFileToString(PluginContent, *PluginDescriptorPath))
+	{
+		// Parse the JSON content
+		TSharedPtr<FJsonObject> JsonObject;
+		TSharedRef<TJsonReader<>> Reader = TJsonReaderFactory<>::Create(PluginContent);
+		if (FJsonSerializer::Deserialize(Reader, JsonObject) && JsonObject.IsValid())
+		{
+			// Create the module array if it doesn't exist
+			if (!JsonObject->HasField(TEXT("Modules")))
+			{
+				TArray<TSharedPtr<FJsonValue>> ModulesArray;
+				TSharedPtr<FJsonObject> ModuleObject = MakeShared<FJsonObject>();
+				ModuleObject->SetStringField(TEXT("Name"), ModName);
+				ModuleObject->SetStringField(TEXT("Type"), TEXT("Runtime"));
+				ModuleObject->SetStringField(TEXT("LoadingPhase"), TEXT("Default"));
+				ModulesArray.Add(MakeShared<FJsonValueObject>(ModuleObject));
+				JsonObject->SetArrayField(TEXT("Modules"), ModulesArray);
+
+				// Write the modified JSON back to the file
+				FString OutputString;
+				TSharedRef<TJsonWriter<>> Writer = TJsonWriterFactory<>::Create(&OutputString);
+				FJsonSerializer::Serialize(JsonObject.ToSharedRef(), Writer);
+				FFileHelper::SaveStringToFile(OutputString, *PluginDescriptorPath);
+			}
+		}
+	}
+
+	// Show popup message
+	FMessageDialog::Open(EAppMsgType::Ok, LOCTEXT("AddCppFilesSuccessMessage", "C++ files created. You must manually build the project for the Development Editor target before you can use them in the Unreal editor. You also must regenerate Visual Studio project files for them to appear in VS/Rider."));
+
 	return FReply::Handled();
 }
 
