ba1b3ef867fd3ae2373ab12d1132ff832f923cb1
Mark Lierman <marklierman@hotmail.com>
2026-05-29 15:22:18 -0400

    Fix for child input mapping
    Add implementation of UFGChildInputMappingContext::GetPrimaryAssetId

--- Source/FactoryGame/Private/Input/FGChildInputMappingContext.cpp
+++ Source/FactoryGame/Private/Input/FGChildInputMappingContext.cpp
@@ -1,3 +1,11 @@
 #include "Input/FGChildInputMappingContext.h"
 
-FPrimaryAssetId UFGChildInputMappingContext::GetPrimaryAssetId() const{ return Super::GetPrimaryAssetId(); }
+FPrimaryAssetId UFGChildInputMappingContext::GetPrimaryAssetId() const
+{
+    // Avoid returning a valid primary asset ID for this class CDO itself
+    if (HasAnyFlags(RF_ClassDefaultObject) && GetClass() == StaticClass())
+    {
+        return FPrimaryAssetId();
+    }
+    return FPrimaryAssetId(StaticClass()->GetFName(), GetFName());
+}
