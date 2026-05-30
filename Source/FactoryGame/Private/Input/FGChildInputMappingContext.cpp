c0ecbc6207b463146ef5734663a25bd54f72e123
Mark Lierman <marklierman@hotmail.com>
2026-05-29 21:59:00 -0400

    Fix for child input mapping "does not match object's real id" error (#419)
    * Fix for child input mapping
    
    Add implementation of UFGChildInputMappingContext::GetPrimaryAssetId
    
    * Add comment with modding edit info + context
    
    ---------
    
    Co-authored-by: Rob B <computerguy440+gh@gmail.com>

--- Source/FactoryGame/Private/Input/FGChildInputMappingContext.cpp
+++ Source/FactoryGame/Private/Input/FGChildInputMappingContext.cpp
@@ -1,3 +1,14 @@
 #include "Input/FGChildInputMappingContext.h"
 
-FPrimaryAssetId UFGChildInputMappingContext::GetPrimaryAssetId() const{ return Super::GetPrimaryAssetId(); }
+FPrimaryAssetId UFGChildInputMappingContext::GetPrimaryAssetId() const
+{
+    // MODDING EDIT Implementation to address "does not match object's real id" error
+    // https://discord.com/channels/555424930502541343/562722670974599227/1509925640747356301
+
+    // Avoid returning a valid primary asset ID for this class CDO itself
+    if (HasAnyFlags(RF_ClassDefaultObject) && GetClass() == StaticClass())
+    {
+        return FPrimaryAssetId();
+    }
+    return FPrimaryAssetId(StaticClass()->GetFName(), GetFName());
+}
