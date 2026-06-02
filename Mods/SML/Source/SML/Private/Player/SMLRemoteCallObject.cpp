513257b28a468b2327c257baf260838828bdaccf
mircearoata <mircearoatapalade@gmail.com>
2026-06-02 18:32:38 +0200

    Fix casing of system chat message

--- Mods/SML/Source/SML/Private/Player/SMLRemoteCallObject.cpp
+++ Mods/SML/Source/SML/Private/Player/SMLRemoteCallObject.cpp
@@ -33,7 +33,7 @@ void USMLRemoteCallObject::SendChatMessage_Implementation(const FString& Message
 		MessageStruct.MessageType = EFGChatMessageType::CMT_CustomMessage;
 		MessageStruct.ServerTimeStamp = GetGameState()->GetServerWorldTimeSeconds();
 		MessageStruct.MessageSenderColor = Color;
-		MessageStruct.MessageSender = FText::FromString(TEXT("System"));
+		MessageStruct.MessageSender = FText::FromString(TEXT("SYSTEM"));
 		ChatManager->AddChatMessageToReceived(MessageStruct);
 	} else {
 		UE_LOG(LogSatisfactoryModLoader, Error, TEXT("A mod tried to send a chat message before the game's ChatManager was ready! It has been prevented to avoid a crash. The mod developer must fix this by waiting for the chat manager to be valid. The message would have been: %s"), *Message);
